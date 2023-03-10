43b78602aa6fca285e47362e1e502b3714769c6a
j3ang <j3ang.info@gmail.com>
2023-03-10 13:20:07 -0600

    init code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# discourse_user_auto_activation
+# User Auto Activation Plugin
+
+Allow a user to be instantly activated when creating an account with an email
+
+# Usage
+
+1. Go to admin/settings/users
+2. Tick auto_activation_enabled checkbox
---
+++ plugin.rb
@@ -0,0 +1,26 @@
+# name: auto activation
+# about: Auto activate new accounts
+# version: 1
+
+
+after_initialize do
+  # Rails Engine for accepting votes.
+  module AutoActivationPlugin
+    require_dependency 'users_controller'
+    require_dependency 'user'
+
+    User.class_eval do
+      def create_email_token
+        email_tokens.create(email: email) unless SiteSetting.auto_activation_enabled
+      end
+    end
+    UsersController.class_eval do
+      private
+
+      def user_params
+        merge_fields = { ip_address: request.ip }
+        merge_fields.merge!(active: true) if SiteSetting.auto_activation_enabled
+      end
+    end
+  end
+end
